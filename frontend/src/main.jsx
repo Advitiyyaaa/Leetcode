d628715e6c7ad4e04ce0c3a3d4d28996c587595a
Advitiya Arya <advitiya.arya@gmail.com>
2026-01-16 18:18:24 +0530

    icon added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter} from "react-router";
 import {store} from "./store/store"
 import {Provider} from "react-redux"
 import App from './App';
